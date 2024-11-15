81e8674a37eb12c354081a203f1b60328f459a5d
Yıldırım Özen <yildirimozew@hotmail.com>
2024-11-15 22:36:45 +0300

    added todo list

---
+++ LLM.py
@@ -0,0 +1,6 @@
+"""what we need our agent to do:
+1- decide on whether to query the database
+2- decide on whether to give reference to the page the info was retrieved from
+3- decide on whether to translate
+4- decide on whether to memorize??
+other than that we need a session key to retrieve chats"""
