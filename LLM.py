16c312b8f41a265fe627195a0559988555de0eed
Yıldırım Özen <yildirimozew@hotmail.com>
2024-11-23 21:32:20 +0300

    Implemented and connected the whole agentic workflow without RAG

--- LLM.py
+++ LLM.py
@@ -42,7 +42,6 @@ from langgraph.graph.message import add_messages
 import json
 from langchain_core.messages import ToolMessage
 
-
 """what we need our agent to do:
 1- decide on whether to query the database
 2- decide on whether to give reference to the page the info was retrieved from
@@ -63,7 +62,7 @@ for file in csv_files:
     
     df.to_sql(table_name, con=engine, index=False, if_exists="replace")
 
-db = SQLDatabase(engine=engine, sample_rows_in_table_info=3)
+db = SQLDatabase(engine=engine, sample_rows_in_table_info=1)
 
 llm = ChatOllama(
     model="llama3.1",
@@ -71,168 +70,76 @@ llm = ChatOllama(
     num_ctx=8000,
 )
 
-#this loads a text file
-#loader = TextLoader("./data/metu.txt")
-#data = loader.load()
-#text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-#all_splits = text_splitter.split_documents(data)
-#local_embeddings = OllamaEmbeddings(model="mxbai-embed-large:latest")
-#vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings)
-#retriever = vectorstore.as_retriever()
-
 execute_query_tool = QuerySQLDataBaseTool(db=db)
-
-
-def print_and_return(string: str) -> str:
-    print(string)
-    return string 
-
-#tool initalization
-@tool 
-def query_database(user_request: str) -> str:
-    """Use this when you need to query the database. Takes natural language user request as input and outputs database results"""
-    #TODO only link relevant columns here
-    template = '''Given an input question, create a syntactically correct SQLite query to run. Only return the query. The sql query should only return {top_k} results.
-    Make sure to only use the provided table and column names.
-    Here are the tables and example rows:
-
-    {table_info}.
-
-    Question: {input}'''
-    prompt = PromptTemplate.from_template(template)
-    write_query = create_sql_query_chain(llm, db, prompt = prompt, k = 5)
-    SQL_query = write_query.invoke({"question": user_request})
-    result = execute_query_tool.invoke(SQL_query)
-    return result
-
-@tool
-def execute_query(SQLquery: str) -> str:
-    """Execute SQL query created by create_query on the database. Returns the result """
-    result = execute_query_tool.invoke(SQLquery)
-    #can do error correction here
-    return result
-
-@tool
-def create_query(query: str) -> str:
-    """Use this when you need Create SQL Query from user request. Takes natural language user request as input and outputs an SQL query."""
-    #TODO only link relevant columns here
-    template = '''Given an input question, create a syntactically correct SQLite query to run. Only return the query. The sql query should only return {top_k} results.
-    Make sure to only use the provided table and column names.
-    Here are the tables and example rows:
-
-    {table_info}.
-
-    Question: {input}'''
-    prompt = PromptTemplate.from_template(template)
-    write_query = create_sql_query_chain(llm, db, prompt = prompt, k = 5)
-    return write_query.invoke({"question": query})
-
-tools = [query_database]
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """You are a very powerful assistant. You can use several tools to help with your answers. Use the tools only when their description tells that they can help with the certain problem.
-            Use the following format:
-
-            Question: the input question you must answer
-            Thought: you should always think about what to do, do not use any tool if it is not needed. 
-            Action: the action to take, should be one of these: None, {tool_names}
-            Action Input: the input to the action
-            Observation: the result of the action
-            ... (this Thought/Action/Action Input/Observation can repeat N times)
-            Thought: I now know the final answer
-            Final Answer: the final answer to the original input question""",
-        ),
-        ("user", "{input}"),
-        ("placeholder", "{agent_scratchpad}"),
-    ]
-)
-
-llm_with_tools = llm.bind_tools(tools)
-
-agent = (
-    {
-        "input": lambda x: x["input"],
-        "agent_scratchpad": lambda x: format_to_openai_tool_messages(
-            x["intermediate_steps"]
-        ),
-        "tool_names": lambda x: " ".join(x.name for x in tools) #TODO fix this
-    }
-    | prompt | print_and_return
-    | llm_with_tools
-    | OpenAIToolsAgentOutputParser()
-)
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=True)
-
-#list(agent_executor.stream({"input": "If Ayşe and Mehmet marry, what will be the relationship between Kemal and Mahmut?"}))
-#bunu dene
-
-#TODO add memory
-
-query = "If Ayşe and Mehmet marry, what will be the relationship between Kemal and Mahmut?"
-
-system_message = "You are a very powerful and helpful assistant."
-
-memory = MemorySaver()
-config = {"configurable": {"thread_id": "test-thread"}}
-
-langgraph_agent_executor = create_react_agent(llm_with_tools, tools, state_modifier=system_message, checkpointer=memory)
-
-#messages = langgraph_agent_executor.invoke({"messages": [("user", query)]}, config=config)
-#{
-#    "input": query,
-#    "output": messages["messages"][-1].content,
-#}
-
-RAG_PROMPT = """
-Use the following context to answer the user's query. If you cannot answer the question, please respond with 'I don't know'.
-
-Question:
-{question}
-
-Context:
-{context}
-"""
-
-rag_prompt = ChatPromptTemplate.from_template(RAG_PROMPT)
-
-#print(messages)
-#bunu da dene
-
 
 #agent tools and langgraph implementation
 
-@tool
-def need_query_database(user_query: str) -> str:
+def need_query_database(user_prompt: str) -> str: 
     """Decides whether to query the database or not"""
-    prompt = """Given a user query, tables in the database, decide whether to query the database to answer question or not.
+    template = PromptTemplate.from_template("""Given a user query, tables in the database, decide whether to query the database to answer question or not.
+    Answer ONLY one word: Yes/No
     Question: {input}
     Tables: {table_info}
-    Answer: Yes/No"""
-    return llm.invoke({"input": user_query, "table_info": db.get_table_info(), "prompt": prompt})
+    Answer: Yes/No""")
+    prompt = template.invoke({"input": user_prompt["messages"][-1].content, "table_info": db.get_table_info()})
+    answer = llm.invoke(prompt)
+    return answer.content
+
+def need_translation(user_prompt: str) -> str:
+    """Decides whether to translate the prompt to English or not"""
+    template = PromptTemplate.from_template("""If the prompt is not in English, you should answer yes. If it is English, answer No.
+    Answer ONLY one word: Yes/No.
+    Prompt: {input}
+    Answer: Yes/No""")
+    prompt = template.invoke({"input": user_prompt["messages"][-1].content})
+    answer = llm.invoke(prompt)
+    return answer.content
+
+def translate_prompt(user_prompt: str) -> str:
+    "Translates the given prompt to English"
+    #TODO implement
+    return user_prompt
+
+def query_database(user_prompt: str) -> str: 
+    """Use this when you need to query the database. Takes natural language user request as input and outputs database results"""
+    #TODO only link relevant columns here
+    template = PromptTemplate.from_template('''Given an input question, create a syntactically correct SQLite query to run. Only return the query. The sql query should only return {top_k} results.
+    Make sure to only use the provided table and column names.
+    Here are the tables and example rows:
+
+    {table_info}.
+
+    Question: {input}''')
+    write_query = create_sql_query_chain(llm, db, prompt = template, k = 5)
+    SQL_query = write_query.invoke({"question": user_prompt["messages"][-1].content})
+    result = execute_query_tool.invoke(SQL_query)
+    answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question. Talk like you're giving your answer to the user who asked the question.
+        Be sure to include the SQL query and result in your answer. Be formal and concise in your response. If an error occurred, return the error message.
+
+    Question: {question}
+    SQL Query: {query} 
+    SQL Result: {result}
+    Answer: """
+    )
+
+    prompt = answer_prompt.invoke({"question": user_prompt["messages"][-1].content, "query": SQL_query, "result": result})
+    answer = llm.invoke(prompt)
+    return {"messages": answer}
 
 
-@tool
-def need_translation(user_query: str) -> str:
-    """Decides whether to translate the query to English or not"""
-    prompt = """Given a user query, decide whether to translate it or not. If the query is not in English, you should translate it to English.
-    Question: {input}
-    Answer: Yes/No"""
-    return llm.invoke({"input": user_query, "prompt": prompt})
-
-tools = [need_query_database, need_translation, query_database]
-llm_with_tools = llm.bind_tools(tools)
+querying_tools = [query_database]
+llm_with_querying = llm.bind_tools(querying_tools)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
+
 graph_builder = StateGraph(State)
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    answer = llm.invoke(state["messages"])
+    return {"messages": answer}
 
 class BasicToolNode:
     """A node that runs the tools requested in the last AIMessage."""
@@ -258,16 +165,42 @@ class BasicToolNode:
                 )
             )
         return {"messages": outputs}
+    
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+def empty_node(state: State):
+    return state
 
 
-tool_node = BasicToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
-
+#nodes
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("translate_prompt", translate_prompt)
+graph_builder.add_node("empty_node", empty_node)
+graph_builder.add_node("query_database", query_database)
 
-graph_builder.add_edge(START, "chatbot")
-
+#edges
+graph_builder.add_edge("translate_prompt", "empty_node")
+graph_builder.add_edge("query_database", END)
 graph_builder.add_edge("chatbot", END)
+
+#conditional edges
+graph_builder.add_conditional_edges(START, need_translation, {"Yes": "translate_prompt", "No": "empty_node"})
+graph_builder.add_conditional_edges("empty_node", need_query_database, {"Yes": "query_database", "No": "chatbot"})
 
 graph = graph_builder.compile()
 
@@ -278,16 +211,11 @@ def stream_graph_updates(user_input: str):
 
 
 while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
-
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
         break
+
+    #stream_graph_updates(user_input)
+    result = graph.invoke({"messages": [("user", user_input)]})["messages"][-1].content
+    print(result)
